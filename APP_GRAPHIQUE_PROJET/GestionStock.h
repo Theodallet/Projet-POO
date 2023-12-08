71326e29d07d49f1ea385329afe32ee6ebda0ccf
Tom LAINE <tom.laine@viacesi.fr>
2023-12-08 08:54:55 +0100

    fin jeudi soir

--- APP_GRAPHIQUE_PROJET/GestionStock.h
+++ APP_GRAPHIQUE_PROJET/GestionStock.h
@@ -143,6 +143,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->AFFICHAGE_STOCK->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			   this->AFFICHAGE_STOCK->Location = System::Drawing::Point(778, 117);
 			   this->AFFICHAGE_STOCK->Name = L"AFFICHAGE_STOCK";
+			   this->AFFICHAGE_STOCK->RowHeadersWidth = 62;
 			   this->AFFICHAGE_STOCK->Size = System::Drawing::Size(412, 550);
 			   this->AFFICHAGE_STOCK->TabIndex = 306;
 			   // 
@@ -152,7 +153,7 @@ namespace APPGRAPHIQUEPROJET {
 				   System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
 			   this->TXT_COULEUR_ARTICLE->Location = System::Drawing::Point(102, 386);
 			   this->TXT_COULEUR_ARTICLE->Name = L"TXT_COULEUR_ARTICLE";
-			   this->TXT_COULEUR_ARTICLE->Size = System::Drawing::Size(286, 26);
+			   this->TXT_COULEUR_ARTICLE->Size = System::Drawing::Size(286, 35);
 			   this->TXT_COULEUR_ARTICLE->TabIndex = 304;
 			   // 
 			   // LOGO
@@ -191,7 +192,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->NomApp->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
 			   this->NomApp->Location = System::Drawing::Point(65, 11);
 			   this->NomApp->Name = L"NomApp";
-			   this->NomApp->Size = System::Drawing::Size(282, 50);
+			   this->NomApp->Size = System::Drawing::Size(425, 74);
 			   this->NomApp->TabIndex = 275;
 			   this->NomApp->Text = L"| Gestion Stock";
 			   // 
@@ -210,7 +211,7 @@ namespace APPGRAPHIQUEPROJET {
 				   static_cast<System::Byte>(0)));
 			   this->TXT_PRIX_ARTICLE->Location = System::Drawing::Point(102, 321);
 			   this->TXT_PRIX_ARTICLE->Name = L"TXT_PRIX_ARTICLE";
-			   this->TXT_PRIX_ARTICLE->Size = System::Drawing::Size(286, 26);
+			   this->TXT_PRIX_ARTICLE->Size = System::Drawing::Size(286, 35);
 			   this->TXT_PRIX_ARTICLE->TabIndex = 282;
 			   // 
 			   // TXT_NOM_ARTICLE
@@ -219,7 +220,7 @@ namespace APPGRAPHIQUEPROJET {
 				   static_cast<System::Byte>(0)));
 			   this->TXT_NOM_ARTICLE->Location = System::Drawing::Point(102, 257);
 			   this->TXT_NOM_ARTICLE->Name = L"TXT_NOM_ARTICLE";
-			   this->TXT_NOM_ARTICLE->Size = System::Drawing::Size(286, 26);
+			   this->TXT_NOM_ARTICLE->Size = System::Drawing::Size(286, 35);
 			   this->TXT_NOM_ARTICLE->TabIndex = 281;
 			   // 
 			   // TITREADD_Stock
@@ -231,7 +232,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->TITREADD_Stock->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
 			   this->TITREADD_Stock->Location = System::Drawing::Point(95, 137);
 			   this->TITREADD_Stock->Name = L"TITREADD_Stock";
-			   this->TITREADD_Stock->Size = System::Drawing::Size(252, 20);
+			   this->TITREADD_Stock->Size = System::Drawing::Size(367, 29);
 			   this->TITREADD_Stock->TabIndex = 293;
 			   this->TITREADD_Stock->Text = L"CARACTERISTIQUES STOCK";
 			   // 
@@ -241,7 +242,7 @@ namespace APPGRAPHIQUEPROJET {
 				   static_cast<System::Byte>(0)));
 			   this->TXT_TVA_ARTICLE->Location = System::Drawing::Point(102, 453);
 			   this->TXT_TVA_ARTICLE->Name = L"TXT_TVA_ARTICLE";
-			   this->TXT_TVA_ARTICLE->Size = System::Drawing::Size(286, 22);
+			   this->TXT_TVA_ARTICLE->Size = System::Drawing::Size(286, 30);
 			   this->TXT_TVA_ARTICLE->TabIndex = 290;
 			   // 
 			   // LABEL_NOM_ARTICLE
@@ -253,7 +254,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->LABEL_NOM_ARTICLE->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
 			   this->LABEL_NOM_ARTICLE->Location = System::Drawing::Point(95, 234);
 			   this->LABEL_NOM_ARTICLE->Name = L"LABEL_NOM_ARTICLE";
-			   this->LABEL_NOM_ARTICLE->Size = System::Drawing::Size(45, 20);
+			   this->LABEL_NOM_ARTICLE->Size = System::Drawing::Size(68, 29);
 			   this->LABEL_NOM_ARTICLE->TabIndex = 283;
 			   this->LABEL_NOM_ARTICLE->Text = L"Nom";
 			   // 
@@ -263,7 +264,7 @@ namespace APPGRAPHIQUEPROJET {
 				   static_cast<System::Byte>(0)));
 			   this->TXT_ID_ARTICLE->Location = System::Drawing::Point(102, 193);
 			   this->TXT_ID_ARTICLE->Name = L"TXT_ID_ARTICLE";
-			   this->TXT_ID_ARTICLE->Size = System::Drawing::Size(286, 26);
+			   this->TXT_ID_ARTICLE->Size = System::Drawing::Size(286, 35);
 			   this->TXT_ID_ARTICLE->TabIndex = 298;
 			   // 
 			   // LABEL_PRIX_ARTICLE
@@ -275,7 +276,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->LABEL_PRIX_ARTICLE->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
 			   this->LABEL_PRIX_ARTICLE->Location = System::Drawing::Point(95, 298);
 			   this->LABEL_PRIX_ARTICLE->Name = L"LABEL_PRIX_ARTICLE";
-			   this->LABEL_PRIX_ARTICLE->Size = System::Drawing::Size(38, 20);
+			   this->LABEL_PRIX_ARTICLE->Size = System::Drawing::Size(58, 29);
 			   this->LABEL_PRIX_ARTICLE->TabIndex = 284;
 			   this->LABEL_PRIX_ARTICLE->Text = L"Prix";
 			   // 
@@ -303,7 +304,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->LABEL_ID_ARTICLE->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
 			   this->LABEL_ID_ARTICLE->Location = System::Drawing::Point(95, 170);
 			   this->LABEL_ID_ARTICLE->Name = L"LABEL_ID_ARTICLE";
-			   this->LABEL_ID_ARTICLE->Size = System::Drawing::Size(84, 20);
+			   this->LABEL_ID_ARTICLE->Size = System::Drawing::Size(119, 29);
 			   this->LABEL_ID_ARTICLE->TabIndex = 297;
 			   this->LABEL_ID_ARTICLE->Text = L"ID Article";
 			   // 
@@ -331,7 +332,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->LABEL8COULEUR_ARTICLE->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
 			   this->LABEL8COULEUR_ARTICLE->Location = System::Drawing::Point(98, 363);
 			   this->LABEL8COULEUR_ARTICLE->Name = L"LABEL8COULEUR_ARTICLE";
-			   this->LABEL8COULEUR_ARTICLE->Size = System::Drawing::Size(71, 20);
+			   this->LABEL8COULEUR_ARTICLE->Size = System::Drawing::Size(105, 29);
 			   this->LABEL8COULEUR_ARTICLE->TabIndex = 286;
 			   this->LABEL8COULEUR_ARTICLE->Text = L"Couleur";
 			   // 
@@ -374,7 +375,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->LABEL_TVA_ARTICLE->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
 			   this->LABEL_TVA_ARTICLE->Location = System::Drawing::Point(98, 430);
 			   this->LABEL_TVA_ARTICLE->Name = L"LABEL_TVA_ARTICLE";
-			   this->LABEL_TVA_ARTICLE->Size = System::Drawing::Size(86, 20);
+			   this->LABEL_TVA_ARTICLE->Size = System::Drawing::Size(126, 29);
 			   this->LABEL_TVA_ARTICLE->TabIndex = 287;
 			   this->LABEL_TVA_ARTICLE->Text = L"Taux TVA";
 			   // 
@@ -386,6 +387,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->BACKGROUND_STOCK_TEXTE->Size = System::Drawing::Size(683, 550);
 			   this->BACKGROUND_STOCK_TEXTE->TabIndex = 305;
 			   this->BACKGROUND_STOCK_TEXTE->TabStop = false;
+			   this->BACKGROUND_STOCK_TEXTE->Click += gcnew System::EventHandler(this, &GestionStock::BACKGROUND_STOCK_TEXTE_Click);
 			   // 
 			   // Background
 			   // 
@@ -403,7 +405,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->label40->ForeColor = System::Drawing::Color::Red;
 			   this->label40->Location = System::Drawing::Point(818, 830);
 			   this->label40->Name = L"label40";
-			   this->label40->Size = System::Drawing::Size(166, 13);
+			   this->label40->Size = System::Drawing::Size(250, 20);
 			   this->label40->TabIndex = 274;
 			   this->label40->Text = L"Message erreur potentielle ajouter";
 			   // 
@@ -537,7 +539,7 @@ namespace APPGRAPHIQUEPROJET {
 		String^ Article_Couleur = this->TXT_COULEUR_ARTICLE->Text;
 		float Article_TVA = Convert::ToSingle(this->TXT_TVA_ARTICLE->Text);
 
-		this->oSvc_Article->ajouter_Article(Article_Nom, Article_Prix, Article_Couleur, Article_TVA);
+		//this->oSvc_Article->ajouter_Article(Article_Nom, Article_Prix, Article_Couleur, Article_TVA);
 
 		this->TXT_NOM_ARTICLE->Text = "";
 		this->TXT_PRIX_ARTICLE->Text = "";
@@ -556,7 +558,7 @@ namespace APPGRAPHIQUEPROJET {
 		String^ Article_Couleur = this->TXT_COULEUR_ARTICLE->Text;
 		float Article_TVA = Convert::ToSingle(this->TXT_TVA_ARTICLE->Text);
 
-		this->oSvc_Article->ajouter_Article(Article_ID, Article_Prix, Article_Nom, Article_Couleur, Article_TVA);
+		//this->oSvc_Article->ajouter_Article(Article_ID, Article_Prix, Article_Nom, Article_Couleur, Article_TVA);
 
 
 		this->TXT_ID_ARTICLE->Text = "";
@@ -602,5 +604,7 @@ namespace APPGRAPHIQUEPROJET {
 		this->TXT_COULEUR_ARTICLE->Text = "Suivant";
 		this->TXT_TVA_ARTICLE->Text = "Suivant";
 	}
+private: System::Void BACKGROUND_STOCK_TEXTE_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
