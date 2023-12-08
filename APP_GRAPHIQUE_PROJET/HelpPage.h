e4879f3f166a2d76308e911c052ee8fa67f5d07b
Polo <120189574+PaulGUYOUMARD@users.noreply.github.com>
2023-12-08 15:53:09 +0100

    modification de maanuel

--- APP_GRAPHIQUE_PROJET/HelpPage.h
+++ APP_GRAPHIQUE_PROJET/HelpPage.h
@@ -56,15 +56,18 @@ namespace APPGRAPHIQUEPROJET {
 	private: System::Windows::Forms::Label^ TitreManuel;
 
 	private: System::Windows::Forms::PictureBox^ BarreNoir;
-	private: System::Windows::Forms::Label^ TEXT_MANUEL;
-	private: System::Windows::Forms::PictureBox^ PLANET;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+
+
+
 
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
 
-	private: System::Windows::Forms::PictureBox^ LOGO;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::PictureBox^ pictureBox2;
-	private: System::Windows::Forms::PictureBox^ Black_Background;
+
+
+
+
 
 #pragma region Windows Form Designer generated code
 		   /// <summary>
@@ -77,31 +80,24 @@ namespace APPGRAPHIQUEPROJET {
 			   this->RETURN_MAUNEL_UTILISATION_HOME = (gcnew System::Windows::Forms::Button());
 			   this->TitreManuel = (gcnew System::Windows::Forms::Label());
 			   this->BarreNoir = (gcnew System::Windows::Forms::PictureBox());
-			   this->TEXT_MANUEL = (gcnew System::Windows::Forms::Label());
-			   this->PLANET = (gcnew System::Windows::Forms::PictureBox());
-			   this->LOGO = (gcnew System::Windows::Forms::PictureBox());
-			   this->Black_Background = (gcnew System::Windows::Forms::PictureBox());
 			   this->label2 = (gcnew System::Windows::Forms::Label());
 			   this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
 			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->BarreNoir))->BeginInit();
-			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->PLANET))->BeginInit();
-			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->LOGO))->BeginInit();
-			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Black_Background))->BeginInit();
 			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			   this->SuspendLayout();
 			   // 
 			   // RETURN_MAUNEL_UTILISATION_HOME
 			   // 
-			   this->RETURN_MAUNEL_UTILISATION_HOME->BackColor = System::Drawing::Color::Black;
-			   this->RETURN_MAUNEL_UTILISATION_HOME->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"RETURN_MAUNEL_UTILISATION_HOME.BackgroundImage")));
+			   this->RETURN_MAUNEL_UTILISATION_HOME->BackColor = System::Drawing::Color::Brown;
 			   this->RETURN_MAUNEL_UTILISATION_HOME->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			   this->RETURN_MAUNEL_UTILISATION_HOME->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
 			   this->RETURN_MAUNEL_UTILISATION_HOME->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Bold));
-			   this->RETURN_MAUNEL_UTILISATION_HOME->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
-			   this->RETURN_MAUNEL_UTILISATION_HOME->Location = System::Drawing::Point(1157, 6);
+			   this->RETURN_MAUNEL_UTILISATION_HOME->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			   this->RETURN_MAUNEL_UTILISATION_HOME->Location = System::Drawing::Point(1133, 6);
 			   this->RETURN_MAUNEL_UTILISATION_HOME->Name = L"RETURN_MAUNEL_UTILISATION_HOME";
-			   this->RETURN_MAUNEL_UTILISATION_HOME->Size = System::Drawing::Size(69, 62);
+			   this->RETURN_MAUNEL_UTILISATION_HOME->Size = System::Drawing::Size(93, 62);
 			   this->RETURN_MAUNEL_UTILISATION_HOME->TabIndex = 243;
+			   this->RETURN_MAUNEL_UTILISATION_HOME->Text = L"RETOUR";
 			   this->RETURN_MAUNEL_UTILISATION_HOME->UseVisualStyleBackColor = false;
 			   this->RETURN_MAUNEL_UTILISATION_HOME->Click += gcnew System::EventHandler(this, &ManuelUtilisation::RETURN_STATS_HOME_Click);
 			   // 
@@ -112,7 +108,7 @@ namespace APPGRAPHIQUEPROJET {
 			   this->TitreManuel->Font = (gcnew System::Drawing::Font(L"Segoe UI", 27.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
 			   this->TitreManuel->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			   this->TitreManuel->Location = System::Drawing::Point(68, 12);
+			   this->TitreManuel->Location = System::Drawing::Point(68, 0);
 			   this->TitreManuel->Name = L"TitreManuel";
 			   this->TitreManuel->Size = System::Drawing::Size(494, 62);
 			   this->TitreManuel->TabIndex = 241;
@@ -121,96 +117,51 @@ namespace APPGRAPHIQUEPROJET {
 			   // BarreNoir
 			   // 
 			   this->BarreNoir->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			   this->BarreNoir->Location = System::Drawing::Point(0, 0);
+			   this->BarreNoir->Location = System::Drawing::Point(-4, -20);
 			   this->BarreNoir->Name = L"BarreNoir";
-			   this->BarreNoir->Size = System::Drawing::Size(1291, 69);
+			   this->BarreNoir->Size = System::Drawing::Size(1291, 759);
 			   this->BarreNoir->TabIndex = 242;
 			   this->BarreNoir->TabStop = false;
-			   // 
-			   // TEXT_MANUEL
-			   // 
-			   this->TEXT_MANUEL->AutoSize = true;
-			   this->TEXT_MANUEL->BackColor = System::Drawing::Color::Black;
-			   this->TEXT_MANUEL->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				   static_cast<System::Byte>(0)));
-			   this->TEXT_MANUEL->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			   this->TEXT_MANUEL->Location = System::Drawing::Point(75, 189);
-			   this->TEXT_MANUEL->Name = L"TEXT_MANUEL";
-			   this->TEXT_MANUEL->Size = System::Drawing::Size(505, 250);
-			   this->TEXT_MANUEL->TabIndex = 259;
-			   this->TEXT_MANUEL->Text = resources->GetString(L"TEXT_MANUEL.Text");
-			   // 
-			   // PLANET
-			   // 
-			   this->PLANET->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"PLANET.Image")));
-			   this->PLANET->Location = System::Drawing::Point(925, 52);
-			   this->PLANET->Name = L"PLANET";
-			   this->PLANET->Size = System::Drawing::Size(348, 687);
-			   this->PLANET->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
-			   this->PLANET->TabIndex = 244;
-			   this->PLANET->TabStop = false;
-			   // 
-			   // LOGO
-			   // 
-			   this->LOGO->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			   this->LOGO->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"LOGO.Image")));
-			   this->LOGO->Location = System::Drawing::Point(12, 12);
-			   this->LOGO->Name = L"LOGO";
-			   this->LOGO->Size = System::Drawing::Size(50, 40);
-			   this->LOGO->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
-			   this->LOGO->TabIndex = 245;
-			   this->LOGO->TabStop = false;
-			   // 
-			   // Black_Background
-			   // 
-			   this->Black_Background->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			   this->Black_Background->Location = System::Drawing::Point(-48, 58);
-			   this->Black_Background->Name = L"Black_Background";
-			   this->Black_Background->Size = System::Drawing::Size(996, 711);
-			   this->Black_Background->TabIndex = 260;
-			   this->Black_Background->TabStop = false;
 			   // 
 			   // label2
 			   // 
 			   this->label2->AutoSize = true;
-			   this->label2->BackColor = System::Drawing::Color::Black;
-			   this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			   this->label2->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			   this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 25.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				   static_cast<System::Byte>(0)));
-			   this->label2->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			   this->label2->Location = System::Drawing::Point(74, 128);
+			   this->label2->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			   this->label2->Location = System::Drawing::Point(395, 282);
 			   this->label2->Name = L"label2";
-			   this->label2->Size = System::Drawing::Size(448, 36);
-			   this->label2->TabIndex = 261;
-			   this->label2->Text = L"Un jour, un grand sage à dit ...\r\n";
+			   this->label2->Size = System::Drawing::Size(425, 60);
+			   this->label2->TabIndex = 244;
+			   this->label2->Text = L"JFUME LA BEUUUH";
 			   // 
 			   // pictureBox2
 			   // 
+			   this->pictureBox2->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			   this->pictureBox2->Cursor = System::Windows::Forms::Cursors::Default;
+			   this->pictureBox2->ErrorImage = nullptr;
 			   this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
-			   this->pictureBox2->Location = System::Drawing::Point(693, 93);
+			   this->pictureBox2->InitialImage = nullptr;
+			   this->pictureBox2->Location = System::Drawing::Point(12, 12);
 			   this->pictureBox2->Name = L"pictureBox2";
-			   this->pictureBox2->Size = System::Drawing::Size(432, 553);
+			   this->pictureBox2->Size = System::Drawing::Size(60, 40);
 			   this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
-			   this->pictureBox2->TabIndex = 262;
+			   this->pictureBox2->TabIndex = 245;
 			   this->pictureBox2->TabStop = false;
+			   this->pictureBox2->Click += gcnew System::EventHandler(this, &ManuelUtilisation::pictureBox2_Click);
 			   // 
 			   // ManuelUtilisation
 			   // 
 			   this->ClientSize = System::Drawing::Size(1256, 725);
 			   this->Controls->Add(this->pictureBox2);
 			   this->Controls->Add(this->label2);
-			   this->Controls->Add(this->LOGO);
 			   this->Controls->Add(this->RETURN_MAUNEL_UTILISATION_HOME);
 			   this->Controls->Add(this->TitreManuel);
 			   this->Controls->Add(this->BarreNoir);
-			   this->Controls->Add(this->TEXT_MANUEL);
-			   this->Controls->Add(this->PLANET);
-			   this->Controls->Add(this->Black_Background);
 			   this->Name = L"ManuelUtilisation";
 			   this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
 			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->BarreNoir))->EndInit();
-			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->PLANET))->EndInit();
-			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->LOGO))->EndInit();
-			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Black_Background))->EndInit();
 			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			   this->ResumeLayout(false);
 			   this->PerformLayout();
@@ -225,5 +176,8 @@ namespace APPGRAPHIQUEPROJET {
 		this->Hide();
 		ObjetManuel->ShowDialog();
 	}
+
+private: System::Void pictureBox2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
