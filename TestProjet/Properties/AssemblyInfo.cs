9ffee35da56372523e1d863734255d20f09f1ecd
Florian BRUNEL <florian.brunel@viacesi.fr>
2023-12-09 23:10:53 +0100

    implementation des tests unitaires

---
+++ TestProjet/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProjet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProjet")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91358781-3dd4-4efd-bd53-d6d05d97ebe6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestProjet/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VotreNamespaceDeTest // Remplacez ceci par votre propre namespace de tests
+{
+    [TestClass]
+    public class CL_Map_ClientTests
+    {
+        [TestMethod]
+        public void Select_Client_Id_Should_Return_Valid_SQL_Query()
+        {
+            // Arrange
+            NS_Comp_Mappage::CL_Map_Client client = new NS_Comp_Mappage::CL_Map_Client();
+
+            // Act
+            string query = client.Select_Client_Id();
+
+            // Assert
+            // Vous pouvez utiliser des assertions pour vérifier si la requête SQL est valide.
+            // Par exemple, vérifier si la chaîne de requête n'est pas vide ou si elle contient des mots-clés attendus.
+            Assert.IsFalse(string.IsNullOrEmpty(query), "La requête SQL ne devrait pas être vide.");
+            Assert.IsTrue(query.Contains("SELECT") && query.Contains("Id"), "La requête doit contenir SELECT et Id.");
+            // Ajoutez d'autres assertions selon vos besoins pour valider la requête SQL.
+        }
+    }
+}
