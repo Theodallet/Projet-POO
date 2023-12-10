1ee176d5072ab5016c32fafa7886a123263fc349
Polo <120189574+PaulGUYOUMARD@users.noreply.github.com>
2023-12-10 10:19:25 +0100

    vérification des entrées pour l'ajout d'un client

--- APP_GRAPHIQUE_PROJET/GestionClient.cpp
+++ APP_GRAPHIQUE_PROJET/GestionClient.cpp
@@ -14,26 +14,25 @@ using namespace std;
 //
 // 1) Méthode pour checker les entrées pour le id du client + éviter les injections SQL
 //
-
 bool APPGRAPHIQUEPROJET::GestionClient::check_client_ID_entry()
 {
 	String^ Client_ID = this->TXT_ID_CLIENT->Text;
 
 	// Convertir System::String^ en std::string
-	std::string clientIDStdString = msclr::interop::marshal_as<std::string>(Client_ID);
+	std::string clientIDString = msclr::interop::marshal_as<std::string>(Client_ID);
 
 	// On vérifie que le texte ne contient que des chiffres
-	for (char c : clientIDStdString) {
+	for (char c : clientIDString) {
 		if (!Char::IsDigit(c)) {
-			MessageBoxA(NULL, "Le champ ID client ne doit contenir que des chiffres.", "Erreur", MB_OK | MB_ICONERROR);
+			MessageBoxA(NULL, "Le champ batiment client ne doit contenir que des chiffres.", "Erreur", MB_OK | MB_ICONERROR);
 			return false;
 		}
 	}
 
 	// On vérifie que le texte ne contient pas de caractères spéciaux pouvant être utilisés pour des injections SQL
-	for (char c : clientIDStdString) {
+	for (char c : clientIDString) {
 		if (c == '"' || c == '\'' || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
-			MessageBoxA(NULL, "Le champ ID client ne doit pas contenir de caractères spéciaux.", "Erreur", MB_OK | MB_ICONERROR);
+			MessageBoxA(NULL, "Le champ nom client ne doit pas contenir de caractères spéciaux.", "Erreur", MB_OK | MB_ICONERROR);
 			return false;
 		}
 	}
@@ -58,7 +57,7 @@ bool APPGRAPHIQUEPROJET::GestionClient::check_client_name_entry()
 	// On vérifie que le texte ne contient que des lettres
 	for (char c : clientNomStdString) {
 		if (!isalpha(c)) {
-			MessageBoxA(NULL, "Le champ nom client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
+			MessageBoxA(NULL, "Le champ 'nom' client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
 			return false;
 		}
 	}
@@ -108,228 +107,221 @@ bool APPGRAPHIQUEPROJET::GestionClient::check_client_surname_entry()
 	// Le texte est valide -> on retourne true
 	return true;
 }
-
-
-
-
-/*
 //
 // 4) Méthode pour checker l' entrée pour la ville du client + éviter les injections SQL
 //
 
-bool check_client_mail_entry()
+bool APPGRAPHIQUEPROJET::GestionClient::check_client_mail_entry()
 {
-	string^ Client_Mail = this->TXT_MAIL_CLIENT->Text;
+	String^ Client_Mail = this->TXT_MAIL_CLIENT->Text;
 
-	// On vérifie que le texte ne contient que des caractères alphanumériques
+	// Convertir System::String^ en std::string
+	std::string clientMailStdString = msclr::interop::marshal_as<std::string>(Client_Mail);
+
+	// On vérifie que le texte ne contient que des caractères alphanumériques et l'@
 	bool is_valid = true;
-	for (char c : Client_Mail) {
-		if (!isalpha(c) && !isdigit(c) && c != '@') {
+	for (char c : clientMailStdString) {
+		if (!isalnum(c) && c != '@') {
 			is_valid = false;
 			break;
 		}
 	}
 
 	if (!is_valid) {
-		MessageBoxA(NULL, "Le champ nom client ne doit contenir que des lettres, des chiffres et l'@.", "Erreur", MB_OK | MB_ICONERROR);
-		return 1;
+		MessageBoxA(NULL, "Le champ mail client ne doit contenir que des lettres, des chiffres et l'@.", "Erreur", MB_OK | MB_ICONERROR);
+		return false;
 	}
 
 	// On vérifie que le texte ne contient pas de caractères spéciaux pouvant être utilisés pour des injections SQL
-	for (char c : Client_Mail) {
-		if (c == '"' || c == "'" || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
-			MessageBoxA(NULL, "Le champ ville client ne doit pas contenir de caractères spéciaux pouvant être utilisés pour des injections SQL.", "Erreur", MB_OK | MB_ICONERROR);
-			return 1;
+	for (char c : clientMailStdString) {
+		if (c == '"' || c == '\'' || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
+			MessageBoxA(NULL, "Le champ mail client ne doit pas contenir de caractères spéciaux pouvant être utilisés pour des injections SQL.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
 	}
 
 	// On vérifie que le texte contient un @
-	if (Client_Mail.IndexOf('@') == -1) {
+	if (Client_Mail->IndexOf('@') == -1) {
 		MessageBoxA(NULL, "Le champ mail doit contenir un @.", "Erreur", MB_OK | MB_ICONERROR);
-		return 1;
+		return false;
 	}
 
 	// On vérifie que le texte contient une extension de mail valide
-	string^ extension = Client_Mail.Substring(Client_Mail.IndexOf('@') + 1);
+	String^ extension = Client_Mail->Substring(Client_Mail->IndexOf(L'@') + 1);
+	std::string clientExtensionStdString = msclr::interop::marshal_as<std::string>(extension);
+
+	// Liste des extensions valides
+	std::vector<std::string> valid_extensions = { ".com", ".fr", ".net", ".org", ".edu" };
+
 	bool is_valid_extension = false;
-	for (string^ extension_valide : { ".com", ".fr", ".net", ".org", ".edu" }) {
-		if (extension == extension_valide) {
+
+	for (const std::string& valid_extension : valid_extensions) {
+		// Comparaison insensible à la casse
+		if (_stricmp(clientExtensionStdString.c_str(), valid_extension.c_str()) == 0) {
 			is_valid_extension = true;
 			break;
 		}
 	}
 
 	if (!is_valid_extension) {
-		MessageBoxA(NULL, "Le champ mail doit contenir une extension valide.", "Erreur", MB_OK | MB_ICONERROR);
-		return 1;
+		MessageBoxW(NULL, L"Le champ mail doit contenir une extension valide.", L"Erreur", MB_OK | MB_ICONERROR);
+		return false;
 	}
 
-	// Le texte est valide -> on retourne le texte
-	char Mail = Client_Mail;
-	return 0;
+	// Le texte est valide -> on retourne true
+	return true;
 }
+
 
 //
 // 6) Méthode pour checker l' entrée pour la ville du client + éviter les injections SQL
 //
 
-bool check_client_ville_entry()
+bool APPGRAPHIQUEPROJET::GestionClient::check_client_ville_entry()
 {
-	string^ Client_ville = this->TXT_VILLE_CLIENT->Text;
+	String^ Client_Ville = this->TXT_VILLE_CLIENT->Text;
 
-	// On vérifie que le texte ne contient que des caractères alphanumériques
-	bool is_valid = true;
-	for (char c : Client_Ville) {
+	// Convertir System::String^ en std::string
+	std::string clientVilleStdString = msclr::interop::marshal_as<std::string>(Client_Ville);
+
+	// On vérifie que le texte ne contient que des lettres
+	for (char c : clientVilleStdString) {
 		if (!isalpha(c)) {
-			is_valid = false;
-			break;
+			MessageBoxA(NULL, "Le champ ville client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
-	}
-
-	if (!is_valid) {
-		MessageBoxA(NULL, "Le champ nom client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
-		return 1;
 	}
 
 	// On vérifie que le texte ne contient pas de caractères spéciaux pouvant être utilisés pour des injections SQL
-	for (char c : Client_Ville) {
-		if (c == '"' || c == "'" || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
-			MessageBoxA(NULL, "Le champ ville client ne doit pas contenir de caractères spéciaux pouvant être utilisés pour des injections SQL.", "Erreur", MB_OK | MB_ICONERROR);
-			return 1;
+	for (char c : clientVilleStdString) {
+		if (c == '"' || c == '\'' || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
+			MessageBoxA(NULL, "Le champ ville client ne doit pas contenir de caractères spéciaux.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
 	}
 
-	// Le texte est valide -> on retourne le texte
-	char Ville = Client_Ville;
-	return 0;
+	// Le texte est valide -> on retourne true
+	return true;
 }
+
 
 //
 // 7) Méthode pour checker l' entrée pour la rue du client + éviter les injections SQL
 //
 
-bool check_client_rue_entry()
+bool APPGRAPHIQUEPROJET::GestionClient::check_client_rue_entry()
 {
-	string^ Client_Rue = this->TXT_RUE_CLIENT->Text;
+	String^ Client_Rue = this->TXT_RUE_CLIENT->Text;
 
-	// On vérifie que le texte ne contient que des caractères alphanumériques
-	bool is_valid = true;
-	for (char c : Client_Rue) {
+	// Convertir System::String^ en std::string
+	std::string clientRuedString = msclr::interop::marshal_as<std::string>(Client_Rue);
+
+	// On vérifie que le texte ne contient que des lettres
+	for (char c : clientRuedString) {
 		if (!isalpha(c)) {
-			is_valid = false;
-			break;
+			MessageBoxA(NULL, "Le champ nom client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
-	}
-
-	if (!is_valid) {
-		MessageBoxA(NULL, "Le champ nom client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
-		return 1;
 	}
 
 	// On vérifie que le texte ne contient pas de caractères spéciaux pouvant être utilisés pour des injections SQL
-	for (char c : Client_Rue{
-		if (c == '"' || c == "'" || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
-			MessageBoxA(NULL, "Le champ ville client ne doit pas contenir de caractères spéciaux pouvant être utilisés pour des injections SQL.", "Erreur", MB_OK | MB_ICONERROR);
-			return 1;
+	for (char c : clientRuedString) {
+		if (c == '"' || c == '\'' || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
+			MessageBoxA(NULL, "Le champ nom client ne doit pas contenir de caractères spéciaux.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
-		}
+	}
 
-		// Le texte est valide -> on retourne le texte
-	char Rue = Client_Rue;
-		return 0;
+	// Le texte est valide -> on retourne true
+	return true;
 }
 
 //
 // 8) Méthode pour checker l' entrée pour le code postal du client + éviter les injections SQL
 //
 
-bool check_client_code_postal_entry()
+bool APPGRAPHIQUEPROJET::GestionClient::check_client_code_postal_entry()
 {
-	string^ Client_Code_Postal = this->TXT_CP_CLIENT->Text;
+	String^ Client_Code_Postal = this->TXT_CP_CLIENT->Text;
 
-	// On vérifie que le texte ne contient que des caractères alphanumériques
-	bool is_valid = true;
-	for (char c : Client_Code_Postal) {
-		if (!isalnum(c)) {
-			is_valid = false;
-			break;
+	// Convertir System::String^ en std::string
+	std::string clientCodePostaldString = msclr::interop::marshal_as<std::string>(Client_Code_Postal);
+
+	// On vérifie que le texte ne contient que des lettres
+	for (char c : clientCodePostaldString) {
+		if (!isalnum(c)) { // VARIANTE AVEC DE l'ALPHANUMERIQUE
+			MessageBoxA(NULL, "Le champ nom client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
 	}
 
-	if (!is_valid) {
-		MessageBoxA(NULL, "Le champ code postal ne doit contenir que des lettres et des chiffres.", "Erreur", MB_OK | MB_ICONERROR);
-		return 1;
+	// On vérifie que le texte ne contient pas de caractères spéciaux pouvant être utilisés pour des injections SQL
+	for (char c : clientCodePostaldString) {
+		if (c == '"' || c == '\'' || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
+			MessageBoxA(NULL, "Le champ nom client ne doit pas contenir de caractères spéciaux.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
+		}
 	}
 
-	// On vérifie que le texte contient 5 caractères
-	if (Client_Code_Postal.Length != 5) {
-		MessageBoxA(NULL, "Le champ code postal doit contenir 5 caractères.", "Erreur", MB_OK | MB_ICONERROR);
-		return 1;
-	}
-
-	// Le texte est valide -> on retourne le texte
-	char Code_Postal = Client_Code_Postal;
-	return 0;
+	// Le texte est valide -> on retourne true
+	return true;
 }
 
 //
 // 9) Méthode pour checker l' entrée pour le nom du bâtiment + éviter les injections SQL
 //
 
-bool check_client_name_building_entry()
+bool APPGRAPHIQUEPROJET::GestionClient::check_client_name_building_entry()
 {
-	string^ Client_Building_Name = this->TXT_NOM_BATIMENT_CLIENT->Text;
+	String^ Client_Batiment= this->TXT_BAT_CLIENT->Text;
 
-	// On vérifie que le texte ne contient que des caractères alphanumériques
-	bool is_valid = true;
-	for (char c : Client_Building_Name) {
-		if (!isalpha(c)) {
-			is_valid = false;
-			break;
+	// Convertir System::String^ en std::string
+	std::string clientBatimentdString = msclr::interop::marshal_as<std::string>(Client_Batiment);
+
+	// On vérifie que le texte ne contient que des lettres
+	for (char c : clientBatimentdString) {
+		if (!isalnum(c)) { // VARIANTE AVEC DE l'ALPHANUMERIQUE
+			MessageBoxA(NULL, "Le champ nom client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
-	}
-
-	if (!is_valid) {
-		MessageBoxA(NULL, "Le champ nom client ne doit contenir que des lettres.", "Erreur", MB_OK | MB_ICONERROR);
-		return 1;
 	}
 
 	// On vérifie que le texte ne contient pas de caractères spéciaux pouvant être utilisés pour des injections SQL
-	for (char c : Client_Building_Name) {
-		if (c == '"' || c == "'" || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
-			MessageBoxA(NULL, "Le champ ville client ne doit pas contenir de caractères spéciaux pouvant être utilisés pour des injections SQL.", "Erreur", MB_OK | MB_ICONERROR);
-			return 1;
+	for (char c : clientBatimentdString) {
+		if (c == '"' || c == '\'' || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
+			MessageBoxA(NULL, "Le champ nom client ne doit pas contenir de caractères spéciaux.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
 	}
 
-	// Le texte est valide -> on retourne le texte
-	char Nom_Bat = Client_Building_Name;
-	return 0;
+	// Le texte est valide -> on retourne true
+	return true;
 }
 
-bool check_client_floor_entry()
+bool APPGRAPHIQUEPROJET::GestionClient::check_client_floor_entry()
 {
-	string^ Client_Floor = this->TXT_ETAGE_CLIENT->Text;
+	String^ Client_Etage = this->TXT_ETAGE_CLIENT->Text;
 
-	// On vérifie que le texte est un entier / ne ocmporte que de chiffres
+	// Convertir System::String^ en std::string
+	std::string clientEtageString = msclr::interop::marshal_as<std::string>(Client_Etage);
 
-	try {
-		int ID_Client = stoi(Client_Floor); // stoi c'est pour transformer une chaîne de caractère en int
-	}
-	catch (invalid_argument& e) {
-		MessageBoxA(NULL, "Le champ ID client doit être un entier.", "Erreur", MB_OK | MB_ICONERROR); 		// Le texte n'est pas un entier donc on ouvre une fenêtre d'erreur
-		return 1;
+	// On vérifie que le texte ne contient que des chiffres
+	for (char c : clientEtageString) {
+		if (!Char::IsDigit(c)) {
+			MessageBoxA(NULL, "Le champ batiment client ne doit contenir que des chiffres.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
+		}
 	}
 
 	// On vérifie que le texte ne contient pas de caractères spéciaux pouvant être utilisés pour des injections SQL
-	for (char c : Client_Floor) {
-		if (c == '"' || c == "'" || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
-			MessageBoxA(NULL, "Le champ ID client ne doit pas contenir de caractères spéciaux pouvant être utilisés pour des injections SQL.", "Erreur", MB_OK | MB_ICONERROR); 			// Le texte contient des caractères spéciaux pouvant être utilisés pour des injections SQL donc on ouvre une fenêtre d'erreur
-			return 1;
+	for (char c : clientEtageString) {
+		if (c == '"' || c == '\'' || c == '\\' || c == ';' || c == '`' || c == '<' || c == '>') {
+			MessageBoxA(NULL, "Le champ nom client ne doit pas contenir de caractères spéciaux.", "Erreur", MB_OK | MB_ICONERROR);
+			return false;
 		}
 	}
 
-	int Etage = stoi(Client_Floor); // Le texte est valide -> on convertit le texte en int
-	return 0;
+	// Le texte est valide -> on retourne true
+	return true;
 }
-*/
+
