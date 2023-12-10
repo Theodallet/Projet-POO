92c3f401f97c15e8c1876aa856a8ec7890a58fbe
Florian BRUNEL <florian.brunel@viacesi.fr>
2023-12-10 10:36:00 +0100

    Implémentation test 2

--- TestProjet/UnitTest1.cs
+++ TestProjet/UnitTest1.cs
@@ -14,15 +14,12 @@ namespace VotreNamespaceDeTest // Remplacez ceci par votre propre namespace de t
         [TestMethod]
         public void Select_Client_Id_Should_Return_Valid_SQL_Query()
         {
-            NS_Comp_Mappage.CL_Map_Client client = NS_Comp_Mappagec.L_Map_Client;    // ????? C'est en public ????
+            NS_Comp_Mappage.CL_Map_Client test = NS_Comp_Mappage.CL_Map_Client;    // ????? C'est en public ????
 
-            
-            string query = client.Select_Client_Id();
+            string query = test.Select_Client_Id();
 
-            
             Assert.IsFalse(string.IsNullOrEmpty(query), "La requête SQL ne devrait pas être vide.");
             Assert.IsTrue(query.Contains("SELECT") && query.Contains("Id"), "La requête doit contenir SELECT et Id.");
-  
         }
     }
 }
