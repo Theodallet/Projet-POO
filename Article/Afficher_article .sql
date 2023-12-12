56d2097fc70c8f53b1da065f70609db9f3226b9e
Theodallet <150134673+Theodallet@users.noreply.github.com>
2023-12-12 11:30:31 +0100

    Delete Article directory

--- Article/Afficher_article .sql
+++
@@ -1,7 +0,0 @@
-use[Projet_POO]
-GO
-
-CREATE PROC Afficher_Article
-
-AS
-	SELECT * FROM Article
--- Article/Afficher_article_Id.sql
+++
@@ -1,8 +0,0 @@
-use[Projet_POO]
-GO
-
-CREATE PROC Afficher_Article_Id
-@Id_Art int 
-
-AS
-	SELECT * FROM Article WHERE Id_Art = @Id_Art
